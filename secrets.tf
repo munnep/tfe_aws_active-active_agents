05438f382d5f0f654cff275f547099e5099ff46a
munnep <patrick.munne@hashicorp.com>
2022-11-10 11:56:39 +0100

    secrets working and the agent checking

--- secrets.tf
+++ secrets.tf
@@ -1,8 +1,9 @@
 data "aws_iam_policy_document" "secretsmanager" {
   statement {
-    actions   = ["secretsmanager:GetSecretValue", "secretsmanager:UpdateSecret"]
+    actions   = ["secretsmanager:GetSecretValue", "secretsmanager:UpdateSecret", "secretsmanager:CreateSecret", "secretsmanager:PutSecretValue"]
     effect    = "Allow"
-    resources = [aws_secretsmanager_secret_version.agent_token_secret.secret_id, aws_secretsmanager_secret_version.admin_token_secret.secret_id]
+    resources = [aws_secretsmanager_secret.test.id, aws_secretsmanager_secret.agent_token_secret.id, aws_secretsmanager_secret.admin_token_secret.id]
+    # resources = [aws_secretsmanager_secret.agent_token_secret.id, aws_secretsmanager_secret.admin_token_secret.id]
     sid       = "AllowSecretsManagerSecretAccess"
   }
 }
@@ -15,32 +16,33 @@ resource "aws_iam_role_policy" "secretsmanager" {
 
 resource "aws_secretsmanager_secret" "agent_token_secret" {
   description = "TFE agent token"
-  name        = "${var.tag_prefix}_agent_token"
+  name        = "${var.tag_prefix}_agent_token3"
 
   recovery_window_in_days = 0
 }
 
-resource "aws_secretsmanager_secret_version" "agent_token_secret" {
-  secret_string = "not_yet_set"
-  secret_id     = aws_secretsmanager_secret.agent_token_secret.id
-}
+# resource "aws_secretsmanager_secret_version" "agent_token_secret" {
+#   secret_string = "not_yet_set"
+#   secret_id     = aws_secretsmanager_secret.agent_token_secret.id
+# }
 
 resource "aws_secretsmanager_secret" "admin_token_secret" {
   description = "TFE admin token"
-  name        = "${var.tag_prefix}_admin_token"
+  name        = "${var.tag_prefix}_admin_token3"
 
   recovery_window_in_days = 0
 }
 
-resource "aws_secretsmanager_secret_version" "admin_token_secret" {
-  secret_string = "not_yet_set"
-  secret_id     = aws_secretsmanager_secret.admin_token_secret.id
+# resource "aws_secretsmanager_secret_version" "admin_token_secret" {
+#   secret_string = "not_yet_set"
+#   secret_id     = aws_secretsmanager_secret.admin_token_secret.id
+# }
+
+
+resource "aws_secretsmanager_secret" "test" {
+  description = "test"
+  name        = "${var.tag_prefix}_test"
+
+  recovery_window_in_days = 0
 }
 
-# data "aws_secretsmanager_secret_version" "secret-version" {
-#   secret_id = aws_secretsmanager_secret.agent_token_secret.id
-
-#   depends_on = [
-#     aws_secretsmanager_secret_version.agent_token_secret
-#   ]
-# }
