d49e3e0b9e742e2d85aa43cd072b82d92d6e77fc
munnep <patrick.munne@hashicorp.com>
2022-11-11 09:54:51 +0100

    added a simplified diagram

--- diagram/diagram_simplified.py
+++ diagram/diagram_simplified.py
@@ -24,38 +24,25 @@ with Diagram(
     loadbalancer1 = ElbApplicationLoadBalancer("Application \n Load Balancer")
     bucket_tfe = SimpleStorageServiceS3Bucket("TFE bucket")
     postgresql = RDSPostgresqlInstance("RDS Instance")
-    redis = ElasticacheForRedis("Redis Instance")        
-    tfe_server1 = EC2("TFE Server")
-    tfe_server2 = EC2("TFE Server")
-    tfe_server3 = EC2("TFE Server")
-    tfe_server4 = EC2("TFE Server")
-    tfe_server5 = EC2("TFE Server")
-    tfe_agent1 = EC2("TFE agent")
-    tfe_agent2 = EC2("TFE agent")
-    tfe_agent3 = EC2("TFE agent")
-    tfe_agent4 = EC2("TFE agent")
-    tfe_agent5 = EC2("TFE agent")
-    tfe_agent6 = EC2("TFE agent")
-    tfe_agent7 = EC2("TFE agent")
-    tfe_agent8 = EC2("TFE agent")
-    tfe_agent9 = EC2("TFE agent")
-    tfe_agent10 = EC2("TFE agent")
- 
+    redis = ElasticacheForRedis("Redis Instance")     
+    with Cluster("TFE"):   
+        tfe_server_group = [EC2("TFE Server"),
+                            EC2("TFE Server"),
+                            EC2("TFE Server"),]
+    with Cluster("Agents"):
+        tfe_agent_group = [EC2("TFE agent"),
+                             EC2("TFE agent"),
+                             EC2("TFE agent"),
+                             EC2("TFE agent"),
+                             EC2("TFE agent")]
+        
     # Diagram
-    user >>  loadbalancer1 >> [tfe_server1, 
-                               tfe_server2,
-                               tfe_server3,
-                               tfe_server4,
-                               tfe_server5] >> [tfe_agent1,
-                                                tfe_agent2,
-                                                tfe_agent3,
-                                                tfe_agent4,
-                                                tfe_agent5,
-                                                tfe_agent6,
-                                                tfe_agent7,
-                                                tfe_agent8,
-                                                tfe_agent9,
-                                                tfe_agent10]
+    user >>  loadbalancer1 >> tfe_server_group
+    tfe_server_group >> bucket_tfe         
+    tfe_server_group >> postgresql  
+    tfe_server_group >> redis                                    
+                               
+    loadbalancer1  << tfe_agent_group 
 
     
 diag
