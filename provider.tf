5255a82db4537e797310e219a2f710d26524a048
munnep <patrick.munne@hashicorp.com>
2022-12-19 14:01:44 +0100

    small changes for issues found testing

--- provider.tf
+++ provider.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "=4.37.0"
     }
     acme = {
       source  = "vancluever/acme"
-      version = "~> 2.5.3"
+      version = "=2.5.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "=4.0.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
     }
   }
 
--- secrets.tf
+++ secrets.tf
@@ -7,6 +7,11 @@ data "aws_iam_policy_document" "secretsmanager" {
   }
 }
 
+resource "random_string" "random_secret_name" {
+  length  = 8
+  special = false
+}
+
 resource "aws_iam_role_policy" "secretsmanager" {
   policy = data.aws_iam_policy_document.secretsmanager.json
   role   = aws_iam_role.role.id
@@ -15,7 +20,7 @@ resource "aws_iam_role_policy" "secretsmanager" {
 
 resource "aws_secretsmanager_secret" "agent_token_secret" {
   description = "TFE agent token"
-  name        = "${var.tag_prefix}_agent_token3"
+  name        = "${var.tag_prefix}_${random_string.random_secret_name.result}_agent_token"
 
   recovery_window_in_days = 0
 }
@@ -23,7 +28,7 @@ resource "aws_secretsmanager_secret" "agent_token_secret" {
 
 resource "aws_secretsmanager_secret" "admin_token_secret" {
   description = "TFE admin token"
-  name        = "${var.tag_prefix}_admin_token3"
+  name        = "${var.tag_prefix}_${random_string.random_secret_name.result}_admin_token"
 
   recovery_window_in_days = 0
 }
